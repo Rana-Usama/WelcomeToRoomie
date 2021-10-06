dd001ae21a46b35730705454732be51997099950
RANA-USAMA <ranausamauet3430@gmail.com>
2021-10-06 21:07:17 +0500

    map added

--- App.js
+++ App.js
@@ -9,15 +9,17 @@ import LoginScreen from './app/screens/auth/Index';
 import MyPageScreen from './app/screens/MyPageScreen';
 import AnnounceScreen from './app/screens/AnnounceScreen';
 import SearchScreen from './app/screens/SearchScreen';
+import MapScreen from './app/screens/MapScreen';
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode="none" initialRouteName="AnnounceScreen">
+      <Stack.Navigator headerMode="none" initialRouteName="MapScreen">
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="SearchScreen" component={SearchScreen} />
+        <Stack.Screen name="MapScreen" component={MapScreen} />
         <Stack.Screen name="AnnounceScreen" component={AnnounceScreen} />
         <Stack.Screen name="MyPageScreen" component={MyPageScreen} />
         <Stack.Screen name="SignUp" component={SignUp} />
---
+++ app/screens/MapScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import MapView from 'react-native-maps';
+
+//component
+import Screen from './../components/Screen';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+import BottomTab from '../components/common/BottomTab';
+
+//config
+import Colors from './../config/Colors';
+
+function MapScreen(props) {
+    return (
+        <Screen style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: Colors.white }}>
+            {/* Top Heading */}
+            <View style={{ height: RFPercentage(12), width: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: Colors.primary }}>
+                <Text style={{ color: Colors.white, fontSize: RFPercentage(4.5) }}>
+                    Map
+                </Text>
+            </View>
+            <View style={styles.container}>
+                <MapView style={styles.map} />
+            </View>
+            {/* Bottom Tab */}
+            <BottomTab />
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        // flex: 1.5,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // marginTop: RFPercentage(2)
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
+
+export default MapScreen;
